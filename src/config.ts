37bbdcf870e329cf0df1dc8f70661455f5bdd949
Sam <81268132+Sackah@users.noreply.github.com>
2023-11-14 13:38:47 +0000

    updates

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = process.env.REACT_APP_BASE_URL;
+export const BASE_URL = "https://ca08-196-61-44-226.ngrok-free.app/";
+// export const BASE_URL = process.env.REACT_APP_BASE_URL;
